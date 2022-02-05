52137adcd6bf93849cc478ac0c93c352212a8500
Vladislav <amcors21@gmail.com>
2022-02-05 20:45:34 +0300

    Задача 5

---
+++ Задача (5)/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти максимальное из трех чисел
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 19876;
+int a2 = 3567;
+int a3 = 199;
+
+int max = Max(a1, a2, a3);
+
+System.Console.WriteLine(max);
