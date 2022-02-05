a8ea329c97f4411d3a772a7985903bcfc116d017
Vladislav <amcors21@gmail.com>
2022-02-05 19:25:23 +0300

    Задача 4 есть нюансы

---
+++ Задача (4)/Program.cs
@@ -0,0 +1,9 @@
+﻿string [] a={"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+int b;
+string c;
+Console.Write("Vote day number:");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+if (1 <= b && b <= 7) System.Console.WriteLine(a[b]);
+else System.Console.WriteLine("Ошибка ввода");
+//Программа неработает
