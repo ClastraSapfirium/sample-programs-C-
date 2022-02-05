f8969bbbb5f0ec7c84aafa605439b927dae76445
Vladislav <amcors21@gmail.com>
2022-02-05 17:22:49 +0300

    Задача 1

---
+++ Задача (1)/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вывести квадрат числа
+double a=0;
+string b;
+Console.Clear();
+Console.Write("Vote number a:");
+b=Console.ReadLine();
+a=Convert.ToDouble(b);
+a=a*a;
+System.Console.WriteLine($"{a}");
