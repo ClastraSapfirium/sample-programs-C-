ac40aa8689527cf890ca7bfe748a8eef176a4e03
Vladislav <amcors21@gmail.com>
2022-02-05 17:29:57 +0300

    Задача 2

---
+++ Задача (2)/Program.cs
@@ -0,0 +1,17 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+double a=0,b=0;
+string c;
+Console.Clear();
+Console.Write("Vote number a:");
+c=Console.ReadLine();
+a=Convert.ToDouble(c);
+
+Console.Write("Vote number b:");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+
+if (a*a==b)
+{
+    System.Console.WriteLine("YES");
+}
+else System.Console.WriteLine("NO");
