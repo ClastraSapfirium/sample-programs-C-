b0d467cacf882ae7f192228ea8a5ce4fab40c897
Vladislav <amcors21@gmail.com>
2022-02-05 20:28:44 +0300

    Задача 4 ИСПРАВЛЕННО

--- Задача (4)/Program.cs
+++ Задача (4)/Program.cs
@@ -1,9 +1,19 @@
-﻿string [] a={"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
-int b;
-string c;
+﻿string a1 = "Понедельник";
+string a2 = "Вторник";
+string a3 = "Среда";
+string a4 = "Четверг";
+string a5 = "Пятница";
+string a6 = "Субота";
+string a7 = "Воскресенье";
+Double a8 =0;
+string a9;
 Console.Write("Vote day number:");
-c=Console.ReadLine();
-b=Convert.ToDouble(c);
-if (1 <= b && b <= 7) System.Console.WriteLine(a[b]);
-else System.Console.WriteLine("Ошибка ввода");
-//Программа неработает
+a9=Console.ReadLine();
+a8=Convert.ToDouble(a9);
+if (a8==1) System.Console.WriteLine(a1);
+if (a8==2) System.Console.WriteLine(a2);
+if (a8==3) System.Console.WriteLine(a3);
+if (a8==4) System.Console.WriteLine(a4);
+if (a8==5) System.Console.WriteLine(a5);
+if (a8==6) System.Console.WriteLine(a6);
+if (a8==7) System.Console.WriteLine(a7);
