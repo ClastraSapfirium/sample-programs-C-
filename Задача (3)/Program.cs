bde6d7984ed279b0d63d83b75cf4667f35c6d02a
Vladislav <amcors21@gmail.com>
2022-02-05 17:34:14 +0300

    Задача 3

---
+++ Задача (3)/Program.cs
@@ -0,0 +1,24 @@
+﻿//Даны два числа. Показать большее и меньшее число
+double a=0,b=0;
+string c;
+Console.Clear();
+Console.Write("Vote number a:");
+c=Console.ReadLine();
+a=Convert.ToDouble(c);
+
+Console.Write("Vote number b:");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+
+if (a>b)
+{
+    System.Console.WriteLine($"{a}>{b}");
+}
+if (a<b)
+{
+    System.Console.WriteLine($"{a}<{b}");
+}
+if (a==b)
+{
+    System.Console.WriteLine($"{a}={b}");
+}
