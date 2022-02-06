591fd2b8b96a311f7a95f50f77e1d74347111cb1
Vladislav <amcors21@gmail.com>
2022-02-06 14:05:49 +0300

    Задача 8

---
+++ Задача (8)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать числа от -N до N
+double a=0, b=0;
+string c;
+Console.Clear();
+Console.Write("Vote number -N:");
+c=Console.ReadLine();
+a=Convert.ToDouble(c);
+
+Console.Write("Vote number -N:");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+
+while (a!=b)
+{
+    a=a+1;
+    System.Console.WriteLine($"{a}");
+}
