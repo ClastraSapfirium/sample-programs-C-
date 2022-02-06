dfa76a1a0d2dddca45702dd71ad326c8669939c0
Vladislav <amcors21@gmail.com>
2022-02-06 14:14:19 +0300

    Задача 9

---
+++ Задача (9)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+double a=1, b=0;
+string c;
+Console.Write("Vote number N:");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+
+while (a!=b)
+{
+    if (a%2==0)
+    {
+        System.Console.WriteLine($"{a}");
+    }
+    a=a+1;
+}
