b30150d45ced4f90e9722b320d1206ee15f5114e
Vladislav <amcors21@gmail.com>
2022-02-06 13:31:54 +0300

    Задача 7

--- Задача (6)/Program.cs
+++ Задача (6)/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿//Написать программу вычисления значения функции y = f(a)
+Console.Write("Левая граница отрезка a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Правая граница отрезка b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Количество точек n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+//Есть нюансы (программа не работает)
---
+++ Задача (7)/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить является ли число чётным
+double a=0;
+string b;
+Console.Clear();
+Console.Write("Vote number a:");
+b=Console.ReadLine();
+a=Convert.ToDouble(b);
+if (a%2==0) System.Console.WriteLine("число четное");
+else System.Console.WriteLine("Число нечетное");
