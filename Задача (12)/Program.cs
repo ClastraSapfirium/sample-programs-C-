7cf884fd66f0a52e1456acdcc5775772ebcd06c0
Vladislav <amcors21@gmail.com>
2022-02-07 12:48:14 +0300

    Задача 12

---
+++ Задача (12)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+double a=0;
+string b;
+bool c = true;
+Console.Clear();
+
+Console.Write("Введите число: ");
+b=Console.ReadLine();
+a=Convert.ToDouble(b);
+
+while (c == true)
+{
+    if (a>9 && a<100)
+    {
+        a=a%10;
+        System.Console.WriteLine($"наибольшая цифра числа {a}");
+        c = false;
+    }
+    else
+    {
+        System.Console.WriteLine("Введенное Вами число не может быть меньше 10  или превышать значение 99.");
+        System.Console.Write("Введите число: ");
+        b = Console.ReadLine();
+        a = Convert.ToDouble(b);
+    }
+}
