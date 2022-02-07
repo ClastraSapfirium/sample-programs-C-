8b01751d2658b5b372569990e8c144268a256b13
Vladislav <amcors21@gmail.com>
2022-02-07 12:32:26 +0300

    Задача 11

---
+++ Задача (11)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать вторую цифру трёхзначного числа
+double a=0;
+string b;
+bool c = true;
+Console.Clear();
+
+Console.Write("Введите число: ");
+b=Console.ReadLine();
+a=Convert.ToDouble(b);
+
+while (c == true)
+{
+    if (a>99 && a<1000)
+    {
+        a=a/10.1;
+        a=Convert.ToInt32(a);
+        System.Console.WriteLine(a%10);
+        c = false;
+    }
+    else
+    {
+        System.Console.WriteLine("Введенное Вами число не может быть меньше 100  или превышать значение 999.");
+        System.Console.Write("Введите число: ");
+        b = Console.ReadLine();
+        a = Convert.ToDouble(b);
+    }
+}
