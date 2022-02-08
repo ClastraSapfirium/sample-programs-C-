d712991b5016639b9ef8047e6ffad20907babde2
Vladislav <amcors21@gmail.com>
2022-02-08 12:18:50 +0300

    done

---
+++ Задача (14)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+double a=0, b=0;
+string c;
+Console.Clear();
+Console.Write("Введите число A: ");
+c=Console.ReadLine();
+a=Convert.ToDouble(c);
+
+Console.Write("ВВедите число которому будет кратно число A: ");
+c=Console.ReadLine();
+b=Convert.ToDouble(c);
+
+if (a%b==0)
+{
+    System.Console.WriteLine($"Число {a} кратно числу {b}");
+}
+else System.Console.WriteLine(a/b);
