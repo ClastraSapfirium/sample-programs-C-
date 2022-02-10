ba4a51a2440e3675a109e7581a9dae9167f35ccf
Vladislav <amcors21@gmail.com>
2022-02-10 10:25:44 +0300

    done

---
+++ Задача (23)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+int a=0,b=1;
+string c;
+Console.Clear();
+
+Console.Write("Введите число: ");
+c=Console.ReadLine();
+a=Convert.ToInt32(c);
+while (b<=a)
+{
+    System.Console.WriteLine($"{b} * {b} = {b*b}");
+    b++;
+}
