6cffd1529c051a41087f411ed09270bc23c5d13b
Vladislav <amcors21@gmail.com>
2022-02-11 01:07:17 +0300

    done

---
+++ Задача (27)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе
+int a=0;
+int index = 0;
+string c;
+Console.Clear();
+
+Console.Write("Введите число: ");
+c=Console.ReadLine();
+a=Convert.ToInt32(c);
+
+while (a!=0)
+{
+    a=a/10;
+    a=Convert.ToInt32(a);
+    index++;
+}
+System.Console.WriteLine($"количество цифр в числе {index}");
