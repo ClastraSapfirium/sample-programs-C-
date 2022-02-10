5396951b4d4f64953a2d8bd9ead1903e51078ade
Vladislav <amcors21@gmail.com>
2022-02-10 10:46:24 +0300

    done

---
+++ Задача (25)/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму чисел от 1 до А
+int a=0,d=1;
+int index = 0;
+int sum = 1;
+string c;
+Console.Clear();
+
+Console.Write("Введите число: ");
+c=Console.ReadLine();
+a=Convert.ToInt32(c);
+while (index!=a)
+{
+    sum = sum +d;
+    d = d+1;
+    index++;
+}
+System.Console.WriteLine($"Сумма чисел от 1 до {a} = {sum-1}");
