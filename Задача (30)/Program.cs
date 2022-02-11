f956ebaee4573d5aeeb868db4691630308783612
Vladislav <amcors21@gmail.com>
2022-02-12 01:41:38 +0300

    done

---
+++ Задача (30)/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int a = 0;
+double b = 0;
+string c;
+Console.Clear();
+System.Console.WriteLine("Для завершения программы введите число 0.");
+System.Console.WriteLine("Введите число: ");
+c = Console.ReadLine();
+a = Convert.ToInt32(c);
+
+while (a!=0)
+{
+    if (a%2 == 0)
+    {
+        b = a*a*a;
+        System.Console.WriteLine($"Куб числа {a} равен: {b}");
+    }
+    System.Console.WriteLine("Введите число: ");
+    c = Console.ReadLine();
+    a = Convert.ToInt32(c);
+}
