5067a1a92c091fc550b6b124c2e3729a52afc39e
Vladislav <amcors21@gmail.com>
2022-02-12 01:22:08 +0300

    done

---
+++ Задача (29)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int a = 0, b = 1;
+string s;
+Console.Clear();
+System.Console.Write("Введите число: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int index = 1; index <= a; index++)
+{
+    b = b * index;
+    System.Console.WriteLine($"{b} * {index}");
+}
