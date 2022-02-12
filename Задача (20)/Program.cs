025342d91d9c05a4c1d372fa8f1210ae7528a711
Vladislav <amcors21@gmail.com>
2022-02-12 13:15:38 +0300

    done

--- Задача (20)/Program.cs
+++ Задача (20)/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
+int a = 0;
+string s;
+Console.Clear();
+System.Console.WriteLine("Задайте номер четверти координатной плоскости: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+while (a>4)
+{
+    System.Console.WriteLine("Номер четверти не должен привышать 4");
+    System.Console.WriteLine("Задайте номер четверти координатной плоскости: ");
+    s = Console.ReadLine();
+    a = Convert.ToInt32(s);
+}
+if(a == 1) System.Console.WriteLine($"Диапазоны возможных координат: по осям Х и Y - от 0 до {double.MaxValue}");
+if(a == 2) System.Console.WriteLine($"Диапазоны возможных координат: по оси Х - от {double.MinValue} до 0; по оси Y - от 0 до {double.MaxValue}");
+if(a == 3) System.Console.WriteLine($"Диапазоны возможных координат: по осям Х и Y - от {double.MinValue} до 0");
+if(a == 4) System.Console.WriteLine($"Диапазоны возможных координат: по оси Х - от 0 до {double.MaxValue}; по оси Y - от {double.MinValue} до 0");
