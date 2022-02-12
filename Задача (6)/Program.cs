670c544e15386d33bf0db6f73d4ea0f8d2406df5
Vladislav <amcors21@gmail.com>
2022-02-12 12:36:39 +0300

    done

--- Задача (6)/Program.cs
+++ Задача (6)/Program.cs
@@ -1,9 +1,8 @@
 ﻿//Написать программу вычисления значения функции y = f(a)
-Console.Write("Левая граница отрезка a = ");
-double a = Convert.ToDouble(Console.ReadLine());
-Console.Write("Правая граница отрезка b = ");
-double b = Convert.ToDouble(Console.ReadLine());
-Console.Write("Количество точек n = ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-//Есть нюансы (программа не работает)
+
+double F(double a)
+{
+    return a+1;
+}
+
+System.Console.WriteLine(F(6));
