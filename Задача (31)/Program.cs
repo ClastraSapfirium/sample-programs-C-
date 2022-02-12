1ca540075984eb7399a9da8e2cda76644d82d271
Vladislav <amcors21@gmail.com>
2022-02-12 13:39:47 +0300

    done

---
+++ Задача (31)/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int [] a = new int [8];
+Console.Clear();
+for(int i = 0;i < a.Length; i++)
+    a[i]=new Random().Next(0,100);
+
+for(int i = 0;i<a.Length;i++)   
+    System.Console.Write($"{a[i]} ");
