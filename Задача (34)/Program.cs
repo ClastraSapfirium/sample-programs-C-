3a29b5d0b8f3405536ec8e5e79a714e5165d4fa2
Vladislav <amcors21@gmail.com>
2022-02-13 11:13:03 +0300

    done

---
+++ Задача (34)/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу замены элементов массива на противоположные
+int [] Arrey = new int [12];
+Console.Clear();
+for(int i=0; i< Arrey.Length; i++)
+{
+    Arrey[i] = new Random().Next(-9, 10);
+    System.Console.Write($"{Arrey[i]} ");
+    Arrey[i] = Arrey[i]*(-1);
+}
+System.Console.WriteLine();
+for (int i = 0; i < Arrey.Length; i++)
+{
+    System.Console.Write($"{Arrey[i]} ");
+}
