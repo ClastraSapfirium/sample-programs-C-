4317a870d6f8e41a90d7dbaff88fd6356b76468e
Vladislav <amcors21@gmail.com>
2022-02-17 17:05:35 +0300

    done

--- Задача (41)/Program.cs
+++ Задача (41)/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить являются ли три числа сторонами треугольника
+Console.Clear();
+double a,b,c;
+string s;
+System.Console.WriteLine("Введите число (a): ");
+s = Console.ReadLine();
+a = Convert.ToDouble(s);
+
+System.Console.WriteLine("Введите число (b): ");
+s = Console.ReadLine();
+b = Convert.ToDouble(s);
+
+System.Console.WriteLine("Введите число (c): ");
+s = Console.ReadLine();
+c = Convert.ToDouble(s);
+bool IsString(double x, double y, double z)
+{
+    bool flag;
+    flag=(a+b>c && a+c>b && b+c>a);
+    return flag;
+}
+System.Console.WriteLine($"{a} {b} {c} - "+IsString(a,b,c));
