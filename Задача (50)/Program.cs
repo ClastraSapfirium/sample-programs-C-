bfbcc2ce38e65cff2ddd0c1fc9eff8125fe79faa
Vladislav <amcors21@gmail.com>
2022-03-03 16:49:07 +0300

    done

---
+++ Задача (50)/Program.cs
@@ -0,0 +1,28 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+int a,b;
+string c,d;
+Console.WriteLine("Введите количество строк массива: ");
+c = Console.ReadLine();
+a= Convert.ToInt32(c);
+Console.WriteLine("Введите количество столбцов массива: ");
+d = Console.ReadLine();
+b= Convert.ToInt32(d);
+
+int [,] particle = new int [a,b];
+for (int i=0; i<particle.GetLength(0); i++)
+{
+    for (int e=0; e<particle.GetLength(1); e++)
+    {
+        particle [i,e]= new Random ().Next(1,100);
+    }
+}
+
+for (int i=0; i<particle.GetLength(0); i++)
+{
+    for (int e=0; e<particle.GetLength(1); e++)
+    {
+        if (particle [i,e]%2==0) particle[i,e]= -particle[i,e];
+        System.Console.Write(particle[i,e]+" ");
+    }
+    System.Console.WriteLine();
+}
