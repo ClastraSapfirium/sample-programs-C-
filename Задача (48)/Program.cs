f79a9da8638e0d7c9bdd634e9c4f89a8502efc9f
Vladislav <amcors21@gmail.com>
2022-03-03 16:33:50 +0300

    not done

---
+++ Задача (48)/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int a,b;
+string c,d;
+Console.WriteLine("Введите количество строк массива: ");
+c = Console.ReadLine();
+a= Convert.ToInt32(c);
+Console.WriteLine("Введите количество столбцов массива: ");
+d = Console.ReadLine();
+b= Convert.ToInt32(d);
+
+int [,] particle = new int [a,b];
+for (int i=0; i<particle.GetLength(0); i++)
+{
+    for (int e=0; e<particle.GetLength(1); e++)
+    {
+        particle [i,e]= new Random ().Next(1,100);
+        System.Console.Write(particle[i,e]+" ");
+    }
+    System.Console.WriteLine();
+}
